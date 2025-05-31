0597addf9148c0ea21bbb323efe9478bcaff6adb
Ethan Yifan Zhu <u7560434@anu.edu.au>
2025-05-31 22:48:04 +1000

    OOP. Separate data fetching, processing and visualisation. Add data folder to store data (avoid downloading the same request multiple times).

--- src/retrieve.py
+++ src/retrieve.py
@@ -2,145 +2,176 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import json
+import os
 
-# dataflowIdentifier String(path)
-# The dataflow identifier in {agencyId},{dataflowId},{version} format.
-dataFlowIdentifier = "ABS,RPPI"
+class DataProcessor:
+    def __init__(self):
+        self.measure = ""
+        self.property_type = ""
+        self.region = ""
+        self.startPeriod = ""
+        self.endPeriod = ""
+        self.file_path = ""
+        self.structures = ""
 
-# dataKey String(path)
-'''
-The key to query data returned.
-Use "all" if you would like to return all data for the dataset.
-To filter data returned provide a data key, containing one or more coded values for each dimension, 
-separated by a dot (dimensions must be in the order they are defined in the data structure).
+    def fetch_data(self, measure, property_type, region, startPeriod, endPeriod):
+        # dataflowIdentifier String(path)
+        # The dataflow identifier in {agencyId},{dataflowId},{version} format.
+        dataFlowIdentifier = "ABS,RPPI"
 
-Wildcarding is supported by omitting values for the dimension.
-The OR operator is supported using the + character. 
-You can combine wildcarding and the OR operator.
-'''
+        # dataKey String(path)
+        '''
+        The key to query data returned.
+        Use "all" if you would like to return all data for the dataset.
+        To filter data returned provide a data key, containing one or more coded values for each dimension, 
+        separated by a dot (dimensions must be in the order they are defined in the data structure).
 
-'''
-Measure
-1: Index Nnumbers
-2: Percentage Change from Previous Period
-3: Percentage Change from Corresponding Quarter of the Previous Year
-'''
-measure = "1"
+        Wildcarding is supported by omitting values for the dimension.
+        The OR operator is supported using the + character. 
+        You can combine wildcarding and the OR operator.
+        '''
 
-'''
-Property type
-1: Attached dwellings
-2: Established houses
-3: Residential property
-'''
-property_type = "1"
+        '''
+        Measure
+        1: Index Nnumbers
+        2: Percentage Change from Previous Period
+        3: Percentage Change from Corresponding Quarter of the Previous Year
+        '''
+        self.measure = measure
 
-'''
-Region
-3GBRI: Greater Brisbane
-2GMEL: Greater Melbourne
-6GHOB: Greater Hobart
-5GPER: Greater Perth
-4GADE: Greater Adelaide
-7GDAR: Greater Darwin
-1GSYD: Greater Sydney
-8ACTE: Australian Capital Territory
-100: Weighted average of eight capital cities
+        '''
+        Property type
+        1: Attached dwellings
+        2: Established houses
+        3: Residential property
+        '''
+        self.property_type = property_type
 
-BELOW ENTRIES ARE NOT USED. ONLY INDECATING WHICH STATE THE CAPITAL CITY BELONGS TO.
-3: Queensland
-AUS: Australia
-2: Victoria
-6: Tasmania
-5: Western Australia
-4: South Australia
-7: Northern Territory
-1: New South Wales
-8: Australian Capital Territory
-'''
-region = ""
+        '''
+        Region
+        3GBRI: Greater Brisbane
+        2GMEL: Greater Melbourne
+        6GHOB: Greater Hobart
+        5GPER: Greater Perth
+        4GADE: Greater Adelaide
+        7GDAR: Greater Darwin
+        1GSYD: Greater Sydney
+        8ACTE: Australian Capital Territory
+        100: Weighted average of eight capital cities
 
-'''
-Frequency
-Q: Quarterly
-'''
-frequency = "Q"
+        BELOW ENTRIES ARE NOT USED. ONLY INDECATING WHICH STATE THE CAPITAL CITY BELONGS TO.
+        3: Queensland
+        AUS: Australia
+        2: Victoria
+        6: Tasmania
+        5: Western Australia
+        4: South Australia
+        7: Northern Territory
+        1: New South Wales
+        8: Australian Capital Territory
+        '''
+        self.region = region
 
-dataKey = measure + "." + property_type + "." + region + "." + frequency
+        '''
+        Frequency
+        Q: Quarterly
+        '''
+        frequency = "Q"
 
-'''
-startPeriod & endPeriod
-string(query)
+        dataKey = self.measure + "." + self.property_type + "." + self.region + "." + frequency
 
-The start period and end period (used to filter on time). This is inclusive. The value can be in the following formats:
+        '''
+        startPeriod & endPeriod
+        string(query)
 
-year: yyyy
-year-quarter: yyyy-Q1 - yyyy-Q4
-'''
-startPeriod = "2011-Q4"
-endPeriod = "2021-Q4"
+        The start period and end period (used to filter on time). This is inclusive. The value can be in the following formats:
 
-url = "https://data.api.abs.gov.au/rest/data/" + dataFlowIdentifier + "/" + dataKey + "?format=jsondata"
-if startPeriod != "":
-    url += "&startPeriod=" + startPeriod
-if endPeriod != "":
-    url += "&endPeriod=" + endPeriod
-# print(url)
-response = requests.get(url)
-data = response.json()
+        year: yyyy
+        year-quarter: yyyy-Q1 - yyyy-Q4
+        '''
+        self.startPeriod = startPeriod
+        self.endPeriod = endPeriod
 
-# Extract structures
-structures = data['data']['structures'][0]
-series_dimensions = structures['dimensions']['series']
-observation_dimensions = structures['dimensions']['observation']
-
-# Create mappings for dimension indices to their labels
-dimension_maps = {}
-for dim in series_dimensions:
-    dim_id = dim['id']
-    values = dim['values']
-    dimension_maps[dim_id] = {str(id): value['name'] for id, value in enumerate(values)}
-
-# Map observation indices to time periods
-time_periods = [value['id'] for value in observation_dimensions[0]['values']]
-
-# Process series data
-records = []
-series_data = data['data']['dataSets'][0]['series']
-for series_key, series_value in series_data.items():
-    # Split the series key to get dimension indices
-    indices = series_key.split(':')
-    series_info = {}
-    for i, dim in enumerate(series_dimensions):
-        dim_id = dim['id']
-        index = indices[i]
-        series_info[dim_id] = dimension_maps[dim_id][index]
-    
-    # Extract observations
-    observations = series_value['observations']
-    for obs_index, obs_value in observations.items():
-        record = series_info.copy()
-        record['TIME_PERIOD'] = time_periods[int(obs_index)]
-        record['OBS_VALUE'] = obs_value[0]
-        records.append(record)
-
-# print(dimension_maps)
-# print(time_periods)
-# print(records)
+        file_name = f"{self.measure}.{self.property_type}.{self.region}.{self.startPeriod}.{self.endPeriod}.json"
+        self.file_path = f"./src/data/{file_name}"
 
 
-# Create a DataFrame
-df = pd.DataFrame(records)
-# Create a new column combining 'REGION' and 'PROPERTY_TYPE'
-df['REGION_PROPERTY'] = df['REGION'] + ' - ' + df['PROPERTY_TYPE']
-hue_order = sorted(df['REGION_PROPERTY'].unique())
-print(df)
-# Plot
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=df, x='TIME_PERIOD', y='OBS_VALUE', hue='REGION_PROPERTY', hue_order=hue_order)
-plt.title(df['MEASURE'][0])
-plt.xlabel('Time Period')
-plt.ylabel(structures['attributes']['series'][0]['values'][0]['name'])
-plt.legend(title='Region-Property type')
-plt.tight_layout()
-plt.show()
+        if os.path.exists(self.file_path):
+            print(f"File already exists: {self.file_path}. Skipping download.")
+            return
+
+        url = "https://data.api.abs.gov.au/rest/data/" + dataFlowIdentifier + "/" + dataKey + "?format=jsondata"
+        if self.startPeriod:
+            url += "&startPeriod=" + self.startPeriod
+        if self.endPeriod:
+            url += "&endPeriod=" + self.endPeriod
+        # print(url)
+        response = requests.get(url)
+        data = response.json()
+
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=2)
+
+        print(f"Data saved to data folder")
+
+    def process_data(self):
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        # Extract structures
+        self.structures = data['data']['structures'][0]
+        series_dimensions = self.structures['dimensions']['series']
+        observation_dimensions = self.structures['dimensions']['observation']
+
+        # Create mappings for dimension indices to their labels
+        dimension_maps = {}
+        for dim in series_dimensions:
+            dim_id = dim['id']
+            values = dim['values']
+            dimension_maps[dim_id] = {str(id): value['name'] for id, value in enumerate(values)}
+
+        # Map observation indices to time periods
+        time_periods = [value['id'] for value in observation_dimensions[0]['values']]
+
+        # Process series data
+        records = []
+        series_data = data['data']['dataSets'][0]['series']
+        for series_key, series_value in series_data.items():
+            # Split the series key to get dimension indices
+            indices = series_key.split(':')
+            series_info = {}
+            for i, dim in enumerate(series_dimensions):
+                dim_id = dim['id']
+                index = indices[i]
+                series_info[dim_id] = dimension_maps[dim_id][index]
+            
+            # Extract observations
+            observations = series_value['observations']
+            for obs_index, obs_value in observations.items():
+                record = series_info.copy()
+                record['TIME_PERIOD'] = time_periods[int(obs_index)]
+                record['OBS_VALUE'] = obs_value[0]
+                records.append(record)
+        return records
+
+    def visualise_data(self, records):
+        # Create a DataFrame
+        df = pd.DataFrame(records)
+        # Create a new column combining 'REGION' and 'PROPERTY_TYPE'
+        df['REGION_PROPERTY'] = df['REGION'] + ' - ' + df['PROPERTY_TYPE']
+        hue_order = sorted(df['REGION_PROPERTY'].unique())
+        # Plot
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(data=df, x='TIME_PERIOD', y='OBS_VALUE', hue='REGION_PROPERTY', hue_order=hue_order)
+        plt.title(df['MEASURE'][0])
+        plt.xlabel('Time Period')
+        plt.ylabel(self.structures['attributes']['series'][0]['values'][0]['name'])
+        plt.legend(title='Region-Property type')
+        plt.tight_layout()
+        plt.show()
+
+dp = DataProcessor()
+dp.fetch_data(measure="1", property_type="1", region="1GSYD", startPeriod="2015-Q1", endPeriod="2020-Q4")
+records = dp.process_data()
+dp.visualise_data(records)
